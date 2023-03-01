67b6825e2d101264e9dcb44fe724e47a38f2111a
Chris Mittell <mittell@hotmail.co.uk>
2023-03-01 16:16:07 +0900

    feat: added image and svg config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import './styles.css';
+import IMAGE from './react.png';
+import LOGO from './logo.svg';
 
 export const App = () => {
-	return <h1>React/TypeScript/Webpack Template</h1>;
+	return (
+		<>
+			<h1>React/TypeScript/Webpack Template</h1>;
+			<img src={IMAGE} alt='Logo' width='300' height='300' />
+			<img src={LOGO} alt='Logo' width='300' />
+		</>
+	);
 };
