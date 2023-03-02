534c14f7de372938c5e2dabee468b8b3c3e76dcf
Chris Mittell <mittell@hotmail.co.uk>
2023-03-02 11:05:40 +0900

    feat: added environment webpack configs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,11 @@ import LOGO from './logo.svg';
 export const App = () => {
 	return (
 		<>
-			<h1>React/TypeScript/Webpack Template</h1>;
+			<h1>
+				React/TypeScript/Webpack Template - {process.env.NODE_ENV} -{' '}
+				{process.env.NODE_ENV}
+			</h1>
+			;
 			<img src={IMAGE} alt='Logo' width='300' height='300' />
 			<img src={LOGO} alt='Logo' width='300' />
 		</>
