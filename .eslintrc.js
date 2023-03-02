3b591e83d2d2d000c1762bc75bb006fa98811b6c
Chris Mittell <mittell@hotmail.co.uk>
2023-03-02 11:48:28 +0900

    feat: added tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,8 @@ module.exports = {
         'plugin:import/warnings',
         'plugin:import/typescript',
         'plugin:jsx-a11y/recommended',
-        // 'plugin:eslint-comments/recommended',
-        // 'prettier/@typescript-eslint',
+        'plugin:eslint-comments/recommended',
+        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
     ],
     rules: {
